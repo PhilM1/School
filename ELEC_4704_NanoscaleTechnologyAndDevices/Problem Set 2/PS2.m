d126eb244d9308c7e1967d31f04999e29408fa1d
Phil <pmasson993@gmail.com>
2019-10-04 13:54:47 -0400

    Initial commit of Problem Set 2/PS2.m for nano.
    Added some constants for Q2.1

---
+++ ELEC_4704_NanoscaleTechnologyAndDevices/Problem Set 2/PS2.m
@@ -0,0 +1,16 @@
+%ELEC 4704 A - Nanoscale Technology and Devices
+%Problem Set 2
+%By: Philippe Masson - 101004564
+%Due: Thursday, October 10th 2019
+
+
+%Problem 2.1: -------------------------------
+%Set some constants
+%Tube Scanner Specs:
+thickness_tube = 1.0; %[mm] wall thickness
+diameter_tube = 10.0; %[mm] outer diameter
+length_tube = 30.0; %[mm] length
+%Tripod Scanner Specs:
+thickness_tripod = 1.0; %[mm] thickness
+length_tripod = 30.0; %[mm] length
+
